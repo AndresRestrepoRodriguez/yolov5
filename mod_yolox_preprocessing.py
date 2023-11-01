14fd8b058f2a2e0c564fd9b8fb7e1521f838ce04
Andres Restrepo <aorestrepor@correo.udistrital.edu.co>
2023-11-01 15:28:38 +0000

    preprocessing function match

---
+++ mod_yolox_preprocessing.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import torch
+from torchvision import transforms
+import torchvision
+import math
+
+def preprocess_function(img: np.ndarray, bboxes: np.ndarray) -> np.ndarray:
+    '''Preprocess function used for YOLOX models'''
+    input_size = (1024, 1024)
+    #img_t = np.stack([img]*3, -1)
+    if len(img.shape) == 3:
+        padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
+    else:
+        padded_img = np.ones(input_size, dtype=np.uint8) * 114
+
+    r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    interp = cv2.INTER_LINEAR if r > 1 else cv2.INTER_AREA
+    resized_img = cv2.resize(
+        img,
+        (math.ceil(img.shape[1] * r), math.ceil(img.shape[0] * r)),
+        interpolation=interp,
+    ).astype(np.uint8)
+    padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+
+    #print(padded_img)
+
+    preprocess = transforms.Compose([
+
+        transforms.ToPILImage(),
+
+        transforms.Grayscale(num_output_channels=3),
+
+        transforms.ToTensor()
+
+    ])
+    new_img = np.array(preprocess(resized_img))
+    #print(new_img)
+    padded_img = new_img.transpose((2, 0, 1))
+    padded_img = np.ascontiguousarray(new_img, dtype=np.float32)
+    #print(padded_img)
+    #print(padded_img.shape)
+    #return padded_img, bboxes*r
+    return torch.from_numpy(padded_img), torch.from_numpy(bboxes*r)
